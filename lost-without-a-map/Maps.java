85a80423d7e79256d8c4f28f3db1b1616e31028f
cagataysen <orhancagataysen@outlook.com>
2023-11-22 08:01:48 +0300

    new array values are doubled added

---
+++ lost-without-a-map/Maps.java
@@ -0,0 +1,9 @@
+public class Maps {
+    public static int[] map(int[] arr) {
+        int[] newArr = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            newArr[i] = arr[i] * 2;
+        }
+        return newArr;
+    }
+}
