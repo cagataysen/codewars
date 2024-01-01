74e05ef3dd380b68f33d126d9afaf6a3d30d8419
cagataysen <orhancagataysen@outlook.com>
2024-01-01 21:26:35 +0300

    using Math.abs() making the integer val positive practised

---
+++ will-be-there-enough-space/Bob.java
@@ -0,0 +1,12 @@
+public class Bob {
+    public static int enough(int cap, int on, int wait){
+        if(cap >= on + wait){
+            return 0;
+        }
+        else{
+            Integer result = cap - (on + wait);
+            return Math.abs(result);
+        }
+
+    }
+}
