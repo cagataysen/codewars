2d9ae54c5165e33c6207c30d07059dbff7b6e5b9
cagataysen <orhancagataysen@outlook.com>
2024-01-15 21:18:07 +0300

    inverting-array method added

---
+++ invert-values/InvertValues.java
@@ -0,0 +1,9 @@
+public class InvertValues {
+    public static int[] invert(int[] array) {
+        int[] newArray = new int[array.length];
+        for(int i = 0; i< array.length; i++ ){
+            newArray[i] = array[i] * -1;
+        }
+        return newArray;
+    }
+}
