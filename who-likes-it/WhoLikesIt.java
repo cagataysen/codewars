8212cc95164c55c36663611ee7121cc1e5341599
Cagatay Sen <orhancagataysen@outlook.com>
2024-10-06 14:59:54 +0200

    who likes it kata 6 solved

---
+++ who-likes-it/WhoLikesIt.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class WhoLikesIt {
+    public static String whoLikesIt(String... names) {
+            int n = names.length;
+
+            switch (n) {
+                case 0:
+                    return "no one likes this";
+
+                case 1:
+                    return names[0] + " likes this";
+
+                case 2:
+                    return names[0] + " and " + names[1] + " like this";
+
+                case 3:
+                    return names[0] + ", " + names[1] + " and " + names[2] + " like this";
+
+                default:
+                    return names[0] + ", " + names[1] + " and " + (n - 2) + " others like this";
+            }
+
+        }
+
+}
+
